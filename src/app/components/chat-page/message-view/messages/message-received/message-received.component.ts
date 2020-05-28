3d51da792d5142bf904810244cac6f1d62da4b47
Devin Leamy <devinleamy@gmail.com>
2020-05-27 21:44:13 -0600

    changed the message display

--- src/app/components/chat-page/message-view/messages/message-received/message-received.component.ts
+++ src/app/components/chat-page/message-view/messages/message-received/message-received.component.ts
@@ -1,4 +1,7 @@
-import { Component } from '@angular/core'
+import { Component, OnInit, OnDestroy } from '@angular/core'
+import { UserService } from "../../../../../services/user"
+import { User } from 'src/app/models/user'
+import { Subscription } from 'rxjs'
 
 @Component({
   selector: "app-message-received",
@@ -6,5 +9,21 @@ import { Component } from '@angular/core'
   styleUrls: ["message-received.component.css"]
 })
 export class MessageReceivedComponent {
+  user: User
+  userSub: Subscription
 
+  constructor(private userService: UserService) {}
+
+  ngOnInit() {
+    this.userSub = this.userService.getUserUpdated()
+      .subscribe(user => {
+        this.user = user
+      })
+    this.userService.getUser()
+  }
+
+  //Avoid memory leaks
+  ngOnDestroy() {
+    this.userSub.unsubscribe()
+  }
 }
--- src/app/components/chat-page/message-view/messages/message-sent/message-sent.component.ts
+++ src/app/components/chat-page/message-view/messages/message-sent/message-sent.component.ts
@@ -1,4 +1,7 @@
-import { Component } from '@angular/core'
+import { Component, OnInit, OnDestroy } from '@angular/core'
+import { User } from "../../../../../models/user"
+import { Subscription } from "rxjs"
+import { UserService } from "../../../../../services/user"
 
 @Component({
   selector: "app-message-sent",
@@ -6,5 +9,21 @@ import { Component } from '@angular/core'
   styleUrls: ["message-sent.component.css"]
 })
 export class MessageSentComponent {
+  user: User
+  userSub: Subscription
 
+  constructor(private userService: UserService) {}
+
+  ngOnInit() {
+    this.userSub = this.userService.getUserUpdated()
+      .subscribe(user => {
+        this.user = user
+      })
+    this.userService.getUser()
+  }
+
+  //Avoid memory leaks
+  ngOnDestroy() {
+    this.userSub.unsubscribe()
+  }
 }
