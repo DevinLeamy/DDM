d940029b68db8bca2be268a5255183d5e9071898
Devin Leamy <devinleamy@gmail.com>
2020-06-18 13:57:46 -0600

    Stuff from yesterday

--- routes/functions/userFunc.js
+++ routes/functions/userFunc.js
@@ -1,7 +1,15 @@
+const fs = require("fs")
+
 //Parses jwt ( "Bearer {token}" ) syntax
 function tokenParser(bearerToken) {
   const token = bearerToken.split(" ")[1]
   return token
 }
 
-module.exports = { tokenParser }
+//Encodes file as base 64 string
+function encodeAsBase64(filePath) {
+  const bitmap = fs.readFileSync(filePath)
+  return new Buffer(bitmap).toString("base64")
+}
+
+module.exports = { tokenParser, encodeAsBase64 }
