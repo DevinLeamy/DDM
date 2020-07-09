3453d8bd99ecfdfdf7a4837081a79846811504ea
Devin Leamy <devinleamy@gmail.com>
2020-07-09 10:25:43 -0600

    Did first round css on chat sub

--- src/app/components/chat-selection/chat-option/chat-option.ts
+++ src/app/components/chat-selection/chat-option/chat-option.ts
@@ -1,13 +1,41 @@
 import { Component, Input } from "@angular/core"
 import { ChatsService } from 'src/app/services/chats'
+import { trigger, state, style, animate, transition } from '@angular/animations';
 
 @Component({
         selector: "app-chat-option",
+        animations: [
+                trigger('expansionBox', [
+                        state('open', style({
+                                backgroundColor: "rgb(30, 40, 50)",
+                                height: "90px",
+                                borderRadius: "5px",
+                                width: "100%"
+                        })),
+                        state('closed', style({
+                                // backgroundColor: "rgb(38, 49, 54)",
+                                height: "40px",
+                                borderRadius: "5px",
+                                width: "100%"
+                        })),
+                                transition('open => closed', [
+                                        animate('0.2s')
+                        ]),
+                                transition('closed => open', [
+                                        animate('0.2s')
+                        ]),
+                ]),
+        ],
         templateUrl: "chat-option.html",
         styleUrls: ["chat-option.css"]
 })
 export class ChatOptionComponent {
         @Input() chatId: string
+        expanded = false;
+
+        toggle() {
+          this.expanded = !this.expanded;
+        }
 
         constructor(private chatsService: ChatsService) {}
 
@@ -24,4 +52,6 @@ export class ChatOptionComponent {
                         subCount: 0
                 }
         }
+
+        //Change expand
 }
